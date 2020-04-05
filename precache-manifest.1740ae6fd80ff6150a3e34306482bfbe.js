ee76481cb427b536a02270316be323a1c13663b3
Omer <omer@isolutions.ba>
2020-04-05 12:04:24 -0700

    Updates

---
+++ precache-manifest.1740ae6fd80ff6150a3e34306482bfbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272a41093f91e201efe0251f7e8c0cdd",
+    "url": "/mupdemo/index.html"
+  },
+  {
+    "revision": "185aaa468844624ef367",
+    "url": "/mupdemo/static/css/main.92d8e05a.chunk.css"
+  },
+  {
+    "revision": "1f2d2259880a7cd5a2a1",
+    "url": "/mupdemo/static/js/2.8f8debfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mupdemo/static/js/2.8f8debfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "185aaa468844624ef367",
+    "url": "/mupdemo/static/js/main.c72d3266.chunk.js"
+  },
+  {
+    "revision": "8d63cc677e161f5fac87",
+    "url": "/mupdemo/static/js/runtime-main.348d20ab.js"
+  }
+]);
